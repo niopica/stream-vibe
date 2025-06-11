b70e2a30b54e59fca5934e61a8503a0139ee0587
Nikita Galanzovskiy <niopica@mail.ru>
2025-06-11 20:51:09 +0500

    added hero section, fix header component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -4,7 +4,10 @@ import classNames from "classnames";
 import Button from "@/components/Button";
 import BurgerButton from "@/components/BurgerButton";
 const Header = (props) => {
-    const { url } = props;
+    const {
+        url,
+        isFixed
+    } = props;
 
     const menuItems = [
         {
@@ -26,7 +29,9 @@ const Header = (props) => {
     ]
 
 
-    return <header className="header" data-js-overlay-menu="">
+    return <header className={classNames('header', {
+        "is-fixed": isFixed
+    })} data-js-overlay-menu="">
         <div className="header__inner container">
             <Logo
                 className="header__logo"
